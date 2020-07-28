e86be94c81fb3f1b3c9986db93f4d12977b37d39
Vladyslav <vladyslav.work@gmail.com>
2020-07-28 14:22:18 +0300

    added more matchers

--- intro/intro.test.js
+++ intro/intro.test.js
@@ -2,4 +2,16 @@ const { sum } = require("./intro");
 
 test("Sum should return sum of two values", () => {
   expect(sum(1, 3)).toBe(4);
+  expect(sum(1, 3)).toEqual(4);
+});
+
+test("Sum should return value correctly comparing to other", () => {
+  expect(sum(3, 6)).toBeGreaterThan(6);
+  expect(sum(3, 6)).toBeGreaterThanOrEqual(9);
+  expect(sum(3, 6)).toBeLessThan(10);
+  expect(sum(3, 6)).toBeLessThanOrEqual(9);
+});
+
+test("Sum should sum two float values correctly", () => {
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
 });

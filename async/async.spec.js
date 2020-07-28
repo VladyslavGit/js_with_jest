e63cec1d5adc55044e80f7f600d40fa53b47cee3
Vladyslav <vladyslav.work@gmail.com>
2020-07-28 22:56:19 +0300

    unit test for async/await and promise

--- async/async.spec.js
+++ async/async.spec.js
@@ -5,4 +5,24 @@ describe("Ajax: echo", () => {
     const result = await Ajax.echo("some data");
     expect(result).toBe("some data");
   });
+
+  test("should return async value with promise", () => {
+    return Ajax.echo("some data").then((data) => {
+      expect(data).toBe("some data");
+    });
+  });
+
+  test("should catch error with async/await", async () => {
+    try {
+      await Ajax.echo();
+    } catch (e) {
+      expect(e.message).toBe("error");
+    }
+  });
+
+  test("should catch error with promise", () => {
+    return Ajax.echo().catch((error) => {
+      expect(error).toBeInstanceOf(Error);
+    });
+  });
 });
